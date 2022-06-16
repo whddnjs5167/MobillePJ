12a7b2d0089636c6010b718ab60dd05acc95babc
whddnjs5167 <whddnjs5167@naver.com>
2022-06-16 14:29:39 +0900

    add Register function and login function

--- app/src/main/java/com/example/mobillepj/MainActivity.java
+++ app/src/main/java/com/example/mobillepj/MainActivity.java
@@ -1,33 +1,85 @@
 package com.example.mobillepj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mLoginBtn;
+    TextView mResigettxt;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
+    private static final String TAG = "MY_TAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = findViewById(R.id.signInButton);
-        Button btn2 = findViewById(R.id.singUpButton);
 
-        btn1.setOnClickListener(view -> {
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //버튼 등록하기
+        mResigettxt = findViewById(R.id.register_txt);
+        mLoginBtn = findViewById(R.id.login_btn);
+        mEmailText = findViewById(R.id.emailEt);
+        mPasswordText = findViewById(R.id.passwordEt);
 
-            Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
-            startActivity(intent);
+
+        //가입 버튼이 눌리면
+        mResigettxt.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //intent함수를 통해 register액티비티 함수를 호출한다.
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+
+            }
         });
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
 
-        btn2.setOnClickListener(view -> {
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
 
-            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
-            startActivity(intent);
-            //hihi
+
+                if(email.isEmpty() || pwd.isEmpty()) {
+                    Toast.makeText(MainActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG,"email : " + email + "\n pwd : " + pwd);
+                }
+                else {
+                    firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(MainActivity.this, ModeActivity.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    Toast.makeText(MainActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                }
+            }
         });
     }
 }
